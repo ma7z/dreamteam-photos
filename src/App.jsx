a8355ac756405b1e64bfa0f1188e0f1941d57585
Mateus Augusto <mateushoneey@outlook.com.br>
2023-04-14 05:25:30 -0300

    fotos

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Images = () => {
+  const numImages = 668;
+  const linkPattern = 'https://cdn.dreamteambot.com.br/cards3/s2-%num%-1.png';
+  const images = [];
+
+  for (let i = 1; i <= numImages; i++) {
+    let num = ('00000' + i).slice(-5);
+    let link = linkPattern.replace('%num%', num);
+    images.push(link);
+  }
+
+  const handleImageError = (event) => {
+    event.target.style.display = 'none';
+  };
+
+  return (
+    <div className="container">
+      {images.map((image, index) => (
+        <img
+          key={index}
+          className="imgs"
+          src={image}
+          onError={handleImageError}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Images;
